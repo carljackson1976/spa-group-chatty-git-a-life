6903b7671df8fb822993b852476ae27765219262
Carl Crawley <carljackson1976@gmail.com>
2016-06-13 14:20:47 -0500

    Added dark theme and large text class toggle

--- loading.js
+++ loading.js
@@ -1,5 +1,10 @@
 var chatty = (function(loadChatty){
 	var messageArray= [];
+    var darkTheme= document.getElementById("dark-theme-option");
+    var largeText= document.getElementById("large-text-option");
+
+
+
  loadChatty.loadMessages = function(){
   var myRequest = new XMLHttpRequest(); 
   myRequest.open("GET","messages.json");
@@ -9,14 +14,34 @@ var chatty = (function(loadChatty){
   for(i=0;i<messageArray.length;i++){
     chatty.writeMessage(i,messageArray[i]);
   }
-
+  loadChatty.setdarkTheme();
+  loadChatty.setlargeText();
    
   });
   }
   loadChatty.getMessages = function(){
   	return messageArray;
   }
+  
+  loadChatty.setdarkTheme = function(){
+    darkTheme.addEventListener("change", function(){
+  document.body.classList.toggle("darkTheme");
+    });
+  }
+  
+
+  loadChatty.setlargeText = function(){
+    largeText.addEventListener("change", function(){
+    let outputMessages = document.getElementById('outputMessage');	
+  	outputMessages.classList.toggle("largeText");
+    });
+  }
+  
+
+
+
   return loadChatty;	
 })(chatty || {});
-
+  
+  
 
