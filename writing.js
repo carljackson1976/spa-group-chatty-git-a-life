5ee30c12cfaf148ed9ca4f8cc959e7b8523d533b
Banks Humphreys <iobg@Bankss-MacBook-Pro.local>
2016-06-13 13:08:51 -0500

    function now writes current date

--- writing.js
+++ writing.js
@@ -1,7 +1,9 @@
 var chatty =(function(writeChatty){
+	var date = new Date();
 	var output = document.getElementById("outputMessage");
 	writeChatty.writeMessage=function(id, message){
-		output.innerHTML += `<div id="message${id}">` + message + date.now()+`</div>`
+		output.innerHTML += `<div id="message${id}">` + message +`<div class="date">`+
+		date.toDateString()+" "+ date.toLocaleTimeString()+`</div><input type="button" value="delete" id="delete${id}"></div>`
 
 	}
 	return writeChatty;
