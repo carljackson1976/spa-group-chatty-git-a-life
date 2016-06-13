5d81dcf516f024098a5ec74282a5ca2e1031b357
Dominic Serrano <dmsrojo@gmail.com>
2016-06-13 15:14:19 -0500

    Fixed the feature that is the last message is deleted by the individual button, disabled delete all button if message output is empty

--- deleting.js
+++ deleting.js
@@ -4,21 +4,23 @@ var chatty = (function(deleteChatty){
 		currentMessage.addEventListener("click",function(){
 			console.log(event.target);
 			//checks if delete button of message is pressed
-			if(event.target.value==="delete"){
-				//deletes message
-				currentMessage.remove();
-			}
-		});
-
-		};
+				if(event.target.value==="delete"){
+					//deletes current message
+					currentMessage.remove();
+					//if the output div is empty it disbles the delete all button
+					if ( document.getElementById('outputMessage').innerHTML === "" ) {
+						console.log("Hey")
+						document.getElementById("deleteMssgsBtn").disabled=true;
+					};//end second if statement
+				};//end first if statement
+			});//end event listener on delete messages for buttons
+		};//end deleteChatty.delete function
 
 		deleteChatty.deleteAll = function(){
 			var output=document.getElementById("outputMessage");
 			output.innerHTML="";
-			
-		}
-		
+		}//end deleteChatty.deleteAll function
+
 	return deleteChatty;
 
 })(chatty || {});
-
