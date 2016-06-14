46b2155c1d32e467487fb4a2a9b144dbb699ce37
Dominic Serrano <dmsrojo@gmail.com>
2016-06-14 15:50:06 -0500

    Added string validation, need to fix chatBot mssg and mssg counter

--- deleting.js
+++ deleting.js
@@ -2,14 +2,12 @@ var chatty = (function(deleteChatty){
 		//accepts message to be deleted
 		deleteChatty.delete=function(currentMessage){
 		currentMessage.addEventListener("click",function(){
-			console.log(event.target);
 			//checks if delete button of message is pressed
 				if(event.target.value==="delete"){
 					//deletes current message
 					currentMessage.remove();
 					//if the output div is empty it disbles the delete all button
 					if ( document.getElementById('outputMessage').innerHTML === "" ) {
-						console.log("Hey")
 						document.getElementById("deleteMssgsBtn").disabled=true;
 					};//end second if statement
 				};//end first if statement
